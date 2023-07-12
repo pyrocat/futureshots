4fa961f737a10fb60b65d511c53e9e0518df3598
leonid-dev <leonid.zvegintsev@gmail.com>
2023-07-12 21:05:36 +0400

    add model drafts

--- src/futureshots/activity_tracker/models.py
+++ src/futureshots/activity_tracker/models.py
@@ -1,3 +1,33 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class JoinUserManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().select_related("user")
+
+
+class ApiLog(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    ip = models.GenericIPAddressField()
+
+    requested_at = models.DateTimeField(db_index=True)
+    response_ms = models.PositiveIntegerField(default=0)
+
+    method = models.CharField(max_length=10)
+    path = models.CharField(max_length=255)
+    query_params = models.TextField(null=True, blank=True)
+    query_data = models.TextField(null=True, blank=True)
+
+    status_code = models.PositiveIntegerField(null=True, blank=True)
+    errors = models.TextField(null=True, blank=True)
+
+    objects = JoinUserManager()
+
+    class Meta:
+        verbose_name = "ApiLog"
+
+    def __str__(self):
+        return f"{self.user.username} {self.method} {self.path}"
--- src/futureshots/users/models.py
+++ src/futureshots/users/models.py
@@ -1,2 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
+
+
+class User(AbstractUser):
+    bio = models.TextField()
+    image = models.ImageField()
+    last_login = models.DateTimeField()
+
+
+class Ban(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    banned_until = models.DateTimeField(null=True)
+
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, related_name="communities", on_delete=models.CASCADE)
+    community = models.ForeignKey(
+        "Community", related_name="users", on_delete=models.CASCADE
+    )
+    position = models.ForeignKey("Position", on_delete=models.DO_NOTHING)
+    date_started = models.DateTimeField(auto_now_add=True)
+
+
+class Community(models.Model):
+    users = models.ManyToManyField(User)
+    name = models.CharField()
+    description = models.TextField()
+
+
+class Position(models.Model):
+    name = models.CharField()
+    permissions = models.JSONField()
